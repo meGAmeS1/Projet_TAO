7ab1b5d4ac862eaab59cfc0244b5ec031b91ece2
tynagmo <tynagmo@hotmail.fr>
2013-02-26 17:07:06 +0100

    commit hello world tynagmo

---
+++ src/Projet_TAO.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Projet_TAO.cpp
+// Author      : Le-Moal/Maginot/Revilliod
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
