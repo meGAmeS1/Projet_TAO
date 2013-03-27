1dcebdbea44aceacd8143e76fdccab580e4f4f48
unknown <thibaut.revilliod@orange.fr>
2013-03-27 22:11:35 +0100

    commentaire ressource, cochon, donut

--- src/Donut.h
+++ src/Donut.h
@@ -16,7 +16,8 @@ using namespace std;
 class Donut: public Ressource {
 public:
 	Donut();
-	Donut(const Position, const string, Monde*, int, int);
+	Donut(const Position, const string, Monde*, int, int); // le constructeur de Donut contient la position du donut, le nom du donut, le monde auquel appartient le donut, la quantité de ressoucre et le nombre de points de vie de la ressource
+
 	virtual ~Donut();
 };
 
