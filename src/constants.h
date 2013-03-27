a9aa4b8f71b5405685305bbb308a4a95f8520706
unknown <thibaut.revilliod@orange.fr>
2013-03-27 22:20:34 +0100

    commentaires constants.h

--- src/constants.h
+++ src/constants.h
@@ -9,17 +9,17 @@
 #define CONSTANTS_H_
 
 /* Elements au démarage */
-const int kNbHomme = 8;
-const int kNbFemme = 8;
-const int kNbDonuts = 5;
-const int kNbCochon = 5;
+const int kNbHomme = 8; // nombre d'homme dans le monde au démarrage de l'application
+const int kNbFemme = 8; // nombre de femmes dans le monde au démarrage de l'application
+const int kNbDonuts = 5; // nombre de donuts dans le monde au démarrage de l'application
+const int kNbCochon = 5; // nombre de cochons dans le monde au démarrage de l'application
 
 /*Monde*/
-const int kLargM = 16; // Largeur - abscisse
-const int kLongM = 16; // Hauteur - ordonnées
+const int kLargM = 16; // Largeur du monde - abscisse
+const int kLongM = 16; // Hauteur du monde - ordonnées
 
 
 /* DIRECTIONS */
-typedef enum {NORD,NORDEST,NORDOUEST,SUD,SUDEST,SUDOUEST} Direction;
+typedef enum {NORD,NORDEST,NORDOUEST,SUD,SUDEST,SUDOUEST} Direction; // type énuméré qui contient les 6 directions possibles pour un déplacement
 
 #endif /* CONSTANTS_H_ */
